d4dadc6e15152427981c4be9789fa2c4451a3e03
Joe Littlejohn <joelittlejohn@gmail.com>
2012-01-16 23:34:27 +0000

    [issue 23] Add ant task (wrapper around cli implementation)

---
+++ jsonschema2pojo-ant/src/main/java/com/googlecode/jsonschema2pojo/ant/Jsonschema2PojoTask.java
@@ -0,0 +1,150 @@
+/**
+ * Copyright © 2010-2011 Nokia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlecode.jsonschema2pojo.ant;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+import com.googlecode.jsonschema2pojo.GenerationConfig;
+import com.googlecode.jsonschema2pojo.cli.Jsonschema2Pojo;
+
+/**
+ * When invoked, this task reads one or more <a
+ * href="http://json-schema.org/">JSON Schema</a> documents and generates DTO
+ * style Java classes for data binding.
+ * <p>
+ * See <a href=
+ * 'http://jsonschema2pojo.googlecode.com'>jsonschema2pojo.googlecode.com</a>.
+ * 
+ * @see <a href="http://ant.apache.org/manual/develop.html">Writing Your Own
+ *      Task</a>
+ */
+public class Jsonschema2PojoTask extends Task implements GenerationConfig {
+
+    private boolean generateBuilders;
+
+    private boolean usePrimitives;
+
+    private File source;
+
+    private File targetDirectory;
+
+    private String targetPackage;
+
+    private boolean skip;
+
+    /**
+     * Execute this task (it's expected that all relevant setters will have been
+     * called by Ant to provide task configuration <em>before</em> this method is called).
+     */
+    public void execute() throws BuildException {
+
+        if (skip) {
+            return;
+        }
+
+        try {
+            Jsonschema2Pojo.generate(this);
+        } catch (IOException e) {
+            throw new BuildException("Error generating classes from JSON Schema file(s) " + source.getPath(), e);
+        }
+    }
+
+    /**
+     * Whether to generate builder-style methods of the form
+     * <code>withXxx(value)</code> (that return <code>this</code>), alongside
+     * the standard, void-return setters.
+     * <p>
+     * Default: <code>false</code>.
+     */
+    public void setGenerateBuilders(boolean generateBuilders) {
+        this.generateBuilders = generateBuilders;
+    }
+
+    /**
+     * Whether to use primitives (<code>long</code>, <code>double</code> ,
+     * <code>boolean</code>) instead of wrapper types where possible when
+     * generating bean properties (has the side-effect of making those
+     * properties non-null).
+     * <p>
+     * Default: <code>false</code>.
+     */
+    public void setUsePrimitives(boolean usePrimitives) {
+        this.usePrimitives = usePrimitives;
+    }
+
+    /**
+     * Location of the JSON Schema file(s). Note: this may refer to a single
+     * file or a directory of files.
+     */
+    public void setSource(File source) {
+        this.source = source;
+    }
+
+    /**
+     * Target directory for generated Java source files.
+     */
+    public void setTargetDirectory(File targetDirectory) {
+        this.targetDirectory = targetDirectory;
+    }
+
+    /**
+     * Package name used for generated Java classes (for types where a fully
+     * qualified name has not been supplied in the schema using the 'javaType'
+     * property).
+     */
+    public void setTargetPackage(String targetPackage) {
+        this.targetPackage = targetPackage;
+    }
+
+    public void setSkip(boolean skip) {
+        this.skip = skip;
+    }
+
+    @Override
+    public boolean isGenerateBuilders() {
+        return generateBuilders;
+    }
+
+    @Override
+    public boolean isUsePrimitives() {
+        return usePrimitives;
+    }
+
+    @Override
+    public File getSource() {
+        return source;
+    }
+
+    @Override
+    public File getTargetDirectory() {
+        return targetDirectory;
+    }
+
+    @Override
+    public String getTargetPackage() {
+        return targetPackage;
+    }
+
+    public boolean isSkip() {
+        return skip;
+    }
+
+}
