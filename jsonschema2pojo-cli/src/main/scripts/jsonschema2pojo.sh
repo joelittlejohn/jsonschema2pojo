215d15552ad68d9c263c3e93e25c0e57f97bbfe6
Joe Littlejohn <joelittlejohn@gmail.com>
2013-02-10 22:42:40 +0000

    Update copyright notice for 2013

--- jsonschema2pojo-cli/src/main/scripts/jsonschema2pojo.sh
+++ jsonschema2pojo-cli/src/main/scripts/jsonschema2pojo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright © 2010-2011 Nokia
+# Copyright © 2010-2013 Nokia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
