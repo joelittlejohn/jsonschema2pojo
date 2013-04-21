2b34a26c87f8715653b2f1cb90d39a9eb5b87aca
Joe Littlejohn <joelittlejohn@gmail.com>
2013-04-21 20:28:23 +0100

    Update library versions

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
                            [org.clojure/data.codec "0.1.0"]
                            [org.clojure/tools.logging "0.2.3"]
                            [compojure "1.1.3"]
-                           [ring/ring-jetty-adapter "1.1.0"]
-                           [com.googlecode.jsonschema2pojo/jsonschema2pojo-core "0.3.3"]]
-            :plugins [[lein-ring "0.7.5"]]
+                           [ring/ring-jetty-adapter "1.1.8"]
+                           [com.googlecode.jsonschema2pojo/jsonschema2pojo-core "0.3.6"]]
+            :plugins [[lein-ring "0.8.3"]]
             :ring {:handler jsonschema2pojo.server/app}
             :main jsonschema2pojo.server
             :min-lein-version "2.0.0")
