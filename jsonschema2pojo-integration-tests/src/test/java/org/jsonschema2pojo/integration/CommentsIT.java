fd2445c1bb265419401918f028f00cd5500fe51d
Joe Littlejohn <joelittlejohn@gmail.com>
2016-07-28 01:14:22 +0100

    Add integration tests for comments in input

---
+++ jsonschema2pojo-integration-tests/src/test/java/org/jsonschema2pojo/integration/CommentsIT.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright © 2010-2014 Nokia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jsonschema2pojo.integration;
+
+import org.jsonschema2pojo.integration.util.Jsonschema2PojoRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class CommentsIT {
+
+    @Rule
+    public Jsonschema2PojoRule schemaRule = new Jsonschema2PojoRule();
+
+    @Test
+    public void jsonSchemaWithCommentsIsParsed() {
+        schemaRule.generateAndCompile("/schema/comments/comments.json", "com.example");
+    }
+
+    @Test
+    public void jsonWithCommentsIsParsed() {
+        schemaRule.generateAndCompile("/json/comments.json", "com.example");
+    }
+
+}
