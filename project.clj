f199f53c5041d9f6131c71fdc6582a564b3cba3c
Joe Littlejohn <joelittlejohn@gmail.com>
2016-12-03 20:38:38 +0000

    Update outdated library versions

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject jsonschema2pojo "0.1.0-SNAPSHOT"
             :description "Web front-end for jsonschema2pojo"
-            :dependencies [[org.clojure/clojure "1.5.1"]
+            :dependencies [[org.clojure/clojure "1.7.0"]
                            [org.clojure/data.codec "0.1.0"]
-                           [org.clojure/tools.logging "0.2.6"]
-                           [compojure "1.1.6"]
-                           [ring/ring-jetty-adapter "1.2.1"]
+                           [org.clojure/tools.logging "0.3.1"]
+                           [compojure "1.5.1"]
+                           [ring/ring-jetty-adapter "1.5.0"]
                            [org.jsonschema2pojo/jsonschema2pojo-core "0.4.23"]]
-            :plugins [[lein-ring "0.8.7"]]
+            :plugins [[lein-ring "0.10.0"]]
             :ring {:handler jsonschema2pojo.server/app}
             :main jsonschema2pojo.server
             :min-lein-version "2.0.0")
