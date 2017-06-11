66e11cad781a5d823e12c800a6c72df0d6a2363d
Joe Littlejohn <joelittlejohn@gmail.com>
2017-06-11 20:48:28 +0100

    License date update

--- jsonschema2pojo-integration-tests/src/test/java/org/jsonschema2pojo/integration/FormatIT.java
+++ jsonschema2pojo-integration-tests/src/test/java/org/jsonschema2pojo/integration/FormatIT.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2010-2014 Nokia
+ * Copyright © 2010-2017 Nokia
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -7,7 +7,7 @@
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless optional by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
--- jsonschema2pojo-integration-tests/src/test/java/org/jsonschema2pojo/integration/config/Moshi1IT.java
+++ jsonschema2pojo-integration-tests/src/test/java/org/jsonschema2pojo/integration/config/Moshi1IT.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2016 Tobias Preuss
+ * Copyright © 2010-2017 Nokia
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
