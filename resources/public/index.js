c77c51a53d431a3c2b4daff587ca5d553606c0dd
Joe Littlejohn <joelittlejohn@gmail.com>
2017-06-26 00:53:52 +0100

    Store settings in a cookie

--- resources/public/index.js
+++ resources/public/index.js
@@ -1,7 +1,50 @@
 var INVALID_CLASS_NAME = /[^0-9a-zA-Z\_\$]/;
 var INVALID_PACKAGE_NAME = /[^0-9a-zA-Z\_\$\.]/;
 
+$.fn.serializeAndEncode = function() {
+  // better than jQuery serialize as it uses %2B for space so is compatible with decodeURIComponent
+  return $.map(this.serializeArray(), function(val) {
+    return [val.name, encodeURIComponent(val.value)].join('=');
+  }).join('&');
+};
+
+function saveCookie() {
+  dataString = $("#form").serializeAndEncode();
+  Cookies.set('formData', dataString, { expires: 365 });
+  return false;
+}
+
+function restoreCookie() {
+  var cookieData = Cookies.get('formData');
+  if (cookieData) {
+    var cookieArray = cookieData.split('&');
+
+    $('#form input:checkbox').prop("checked", false);
+
+    $.each(cookieArray, function(k, v) {
+      var field = v.split('=');
+      var fieldName = field[0];
+      var fieldValue = decodeURIComponent(field[1]);
+
+      if (fieldName !== "schema") {
+        var input = $('#form [name="'+fieldName+'"]');
+
+        if ( input.prop("type") == "radio" ) {
+          $('#form [name="'+fieldName+'"][value="' + fieldValue + '"]').prop("checked", true);
+        } else if ( input.prop("type") == "checkbox" ) {
+          input.prop("checked", true);
+        } else {
+          console.log(fieldValue);
+          input.val(fieldValue);
+        }
+      }
+    });
+  }
+}
+
 $(document).ready(function() {
+
+  restoreCookie();
 
   $("#targetpackage").keyup(function(e) {
     if (!$(this).val() || INVALID_PACKAGE_NAME.test($(this).val())) {
@@ -46,7 +89,7 @@ $(document).ready(function() {
     $.ajax({
       url: "generator",
       type: "POST",
-      data: $("#form").serialize(),
+      data: $("#form").serializeAndEncode(),
       success: function(data) {
         $("#download-zip-link").attr("href", "data:application/zip;base64," + data);
         $("#download-zip-link").attr("download", $("#classname").val() + "-sources.zip");
@@ -74,7 +117,7 @@ $(document).ready(function() {
     $.ajax({
       url: "generator/preview",
       type: "POST",
-      data: $("#form").serialize(),
+      data: $("#form").serializeAndEncode(),
       success: function(data) {
         $("#preview-button").button("reset");
         CodeMirror.runMode(data,
@@ -92,5 +135,8 @@ $(document).ready(function() {
       }
     });
   });
+
+  $(document).on("click", "#preview-button", saveCookie);
+  $(document).on("click", "#zip-button", saveCookie);
 
 });
