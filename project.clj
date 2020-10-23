ae2581cc7bf516a35b7031417881346d569bb0a8
Joe Littlejohn <joe.littlejohn@kaluza.com>
2020-10-23 18:35:09 +0100

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject jsonschema2pojo "0.1.0-SNAPSHOT"
   :description "Web front-end for jsonschema2pojo"
-  :dependencies [[compojure "1.5.2"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/tools.logging "0.3.1"]
+  :dependencies [[compojure "1.6.2"]
+                 [org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.codec "0.1.1"]
+                 [org.clojure/tools.logging "0.6.0"]
                  [org.jsonschema2pojo/jsonschema2pojo-core "0.5.1"]
-                 [ring/ring-jetty-adapter "1.5.1"]]
-  :plugins [[lein-ring "0.11.0"]]
+                 [ring/ring-jetty-adapter "1.8.2"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler jsonschema2pojo.server/app}
   :main jsonschema2pojo.server
   :aot [jsonschema2pojo.server]
