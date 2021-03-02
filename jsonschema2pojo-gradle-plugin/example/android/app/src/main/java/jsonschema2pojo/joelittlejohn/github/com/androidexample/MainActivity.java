9361e40ca02b4dd0fb8b14ef5522c646024ec878
Hannes Achleitner <hannes.achleitner@googlemail.com>
2021-03-02 11:44:42 +0100

    Modernize Android sample (#1203)
    * Cleanup
    * Android Studio 4.1
    * Bump Gradle
    * Api 30
    * Android X
    * Format code
    * Github action for Android sample
    * Github action for Gradle verify on Android sample
    * Github action to keep Android sample up to date

--- jsonschema2pojo-gradle-plugin/example/android/app/src/main/java/jsonschema2pojo/joelittlejohn/github/com/androidexample/MainActivity.java
+++ jsonschema2pojo-gradle-plugin/example/android/app/src/main/java/jsonschema2pojo/joelittlejohn/github/com/androidexample/MainActivity.java
@@ -1,12 +1,12 @@
-/**
+/*
  * Copyright © 2010-2020 Nokia
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,8 @@
 
 package jsonschema2pojo.joelittlejohn.github.com.androidexample;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,14 +26,12 @@ import com.oosocial.clarityn.rest.clarityn.model.Entry_schema;
 import com.oosocial.clarityn.rest.clarityn.model.Storage;
 
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
     Entry_schema schema;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -44,24 +43,21 @@ public class MainActivity extends AppCompatActivity
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
+        if (id == R.id.action_settings) {
             return true;
         }
 
