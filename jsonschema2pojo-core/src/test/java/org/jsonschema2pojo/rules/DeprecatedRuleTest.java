8401f2299323cbebc19f75af875cf1d1eb952c2b
Hermann Gruber <HermannGruber@users.noreply.github.com>
2025-08-28 15:46:52 +0200

    Add tests for includeDeprecatedAnnotations

---
+++ jsonschema2pojo-core/src/test/java/org/jsonschema2pojo/rules/DeprecatedRuleTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright © 2010-2020 Nokia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jsonschema2pojo.rules;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.BooleanNode;
+import com.sun.codemodel.JClassAlreadyExistsException;
+import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JDocCommentable;
+
+public class DeprecatedRuleTest {
+
+    private static final String TARGET_CLASS_NAME = DeprecatedRuleTest.class.getName() + ".DummyClass";
+
+    private final DeprecatedRule rule = new DeprecatedRule(new RuleFactory());
+
+    @Test
+    public void applyAddsTextWhenDeprecated() throws JClassAlreadyExistsException {
+
+        JDefinedClass jclass = new JCodeModel()._class(TARGET_CLASS_NAME);
+
+        ObjectMapper mapper = new ObjectMapper();
+        BooleanNode deprecatedNode = mapper.createObjectNode().booleanNode(true);
+
+        JDocCommentable result = rule.apply("fooBar", deprecatedNode, null, jclass, null);
+
+        assertThat(result.javadoc(), sameInstance(jclass.javadoc()));
+        assertThat(result.javadoc().size(), is(1));
+        assertThat((String) result.javadoc().get(0), is("\n@deprecated"));
+
+    }
+
+    @Test
+    public void applySkipsTextWhenNotDeprecated() throws JClassAlreadyExistsException {
+
+        JDefinedClass jclass = new JCodeModel()._class(TARGET_CLASS_NAME);
+
+        ObjectMapper mapper = new ObjectMapper();
+        BooleanNode deprecatedNode = mapper.createObjectNode().booleanNode(false);
+
+        JDocCommentable result = rule.apply("fooBar", deprecatedNode, null, jclass, null);
+
+        assertThat(result.javadoc(), sameInstance(jclass.javadoc()));
+        assertThat(result.javadoc().size(), is(0));
+    }
+
+}
