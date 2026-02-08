ade80f676da19348aa54058ec522d3833f65f719
Andy Boothe <andy.boothe@gmail.com>
2026-02-08 10:49:21 +0000

    Fix missing @Size annotations on arrays
    - Fix check for array type by avoiding ClassNotFoundException thrown by
      Class.forName
    - Add integration test using a byte[] with minLength and maxLength
    - Remove unit tests that are using codemodel mocks.
    
    Closes #1669.
    
    Co-authored-by: Joe Littlejohn <joelittlejohn@gmail.com>

--- jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/model/JAnnotatedClass.java
+++ jsonschema2pojo-core/src/main/java/org/jsonschema2pojo/model/JAnnotatedClass.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 
 import com.sun.codemodel.JAnnotationUse;
 import com.sun.codemodel.JClass;
